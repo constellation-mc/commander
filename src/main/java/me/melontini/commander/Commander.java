5f744956d9b64272d0271501bb23ba74fbc733fd
melontini <104443436+melontini@users.noreply.github.com>
2024-03-20 21:27:28 +0700

    Very initial commit

---
+++ src/main/java/me/melontini/commander/Commander.java
@@ -0,0 +1,11 @@
+package me.melontini.commander;
+
+import net.fabricmc.api.ModInitializer;
+
+public class Commander implements ModInitializer {
+
+    @Override
+    public void onInitialize() {
+
+    }
+}
