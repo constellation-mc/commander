c5bfaf0401fc694d5268f0800673db94a397622f
melontini <104443436+melontini@users.noreply.github.com>
2024-03-31 20:12:19 +0700

    Remove the unused Extractor interface

--- src/main/java/me/melontini/commander/builtin/BuiltInSelectors.java
+++ src/main/java/me/melontini/commander/builtin/BuiltInSelectors.java
@@ -1,7 +1,7 @@
 package me.melontini.commander.builtin;
 
 import lombok.experimental.UtilityClass;
-import me.melontini.commander.command.selector.Extractor;
+import me.melontini.commander.command.selector.MacroBuilder;
 import me.melontini.commander.command.selector.Selector;
 import me.melontini.dark_matter.api.base.util.Utilities;
 import me.melontini.dark_matter.api.minecraft.util.TextUtil;
@@ -28,10 +28,10 @@ public final class BuiltInSelectors {
                 world, 4, world.getRegistryKey().getValue().toString(), TextUtil.literal(world.getRegistryKey().getValue().toString()),
                 world.getServer(), null);
     });
-    public static final Selector THIS_ENTITY = Selector.register(mc("this_entity"), context -> forEntity(context.lootContext().requireParameter(LootContextParameters.THIS_ENTITY)), Extractor.forEntity());
-    public static final Selector KILLER_ENTITY = Selector.register(mc("killer_entity"), context -> forEntity(context.lootContext().requireParameter(LootContextParameters.KILLER_ENTITY)), Extractor.forEntity());
-    public static final Selector DIRECT_KILLER_ENTITY = Selector.register(mc("direct_killer_entity"), context -> forEntity(context.lootContext().requireParameter(LootContextParameters.DIRECT_KILLER_ENTITY)), Extractor.forEntity());
-    public static final Selector LAST_DAMAGE_PLAYER = Selector.register(mc("last_damage_player"), context -> forEntity(context.lootContext().requireParameter(LootContextParameters.LAST_DAMAGE_PLAYER)), Extractor.forEntity());
+    public static final Selector THIS_ENTITY = Selector.register(mc("this_entity"), context -> forEntity(context.lootContext().requireParameter(LootContextParameters.THIS_ENTITY)), MacroBuilder.forEntity());
+    public static final Selector KILLER_ENTITY = Selector.register(mc("killer_entity"), context -> forEntity(context.lootContext().requireParameter(LootContextParameters.KILLER_ENTITY)), MacroBuilder.forEntity());
+    public static final Selector DIRECT_KILLER_ENTITY = Selector.register(mc("direct_killer_entity"), context -> forEntity(context.lootContext().requireParameter(LootContextParameters.DIRECT_KILLER_ENTITY)), MacroBuilder.forEntity());
+    public static final Selector LAST_DAMAGE_PLAYER = Selector.register(mc("last_damage_player"), context -> forEntity(context.lootContext().requireParameter(LootContextParameters.LAST_DAMAGE_PLAYER)), MacroBuilder.forEntity());
     public static final Selector BLOCK_ENTITY = Selector.register(mc("block_entity"), context -> {
         var be = context.lootContext().requireParameter(LootContextParameters.BLOCK_ENTITY);
         return new ServerCommandSource(context.lootContext().getWorld().getServer(), Vec3d.ofCenter(be.getPos()), Vec2f.ZERO,
@@ -42,17 +42,17 @@ public final class BuiltInSelectors {
     public static final Selector DAMAGE_SOURCE_SOURCE = Selector.register(id("damage_source/source"), context -> {
         var s = context.lootContext().requireParameter(LootContextParameters.DAMAGE_SOURCE).getSource();
         return s != null ? forEntity(s) : null;
-    }, Extractor.forEntity());
+    }, MacroBuilder.forEntity());
     public static final Selector DAMAGE_SOURCE_ATTACKER = Selector.register(id("damage_source/attacker"), context -> {
         var s = context.lootContext().requireParameter(LootContextParameters.DAMAGE_SOURCE).getAttacker();
         return s != null ? forEntity(s) : null;
-    }, Extractor.forEntity());
+    }, MacroBuilder.forEntity());
 
     public static final Selector RANDOM_PLAYER = Selector.register(id("random_player"), context -> {
         var l = context.lootContext().getWorld().getServer().getPlayerManager().getPlayerList();
         if (l == null || l.isEmpty()) return null;
         return forEntity(Utilities.pickAtRandom(l));
-    }, Extractor.forEntity());
+    }, MacroBuilder.forEntity());
 
     public static void init() {
     }
--- src/main/java/me/melontini/commander/command/selector/MacroBuilder.java
+++ src/main/java/me/melontini/commander/command/selector/MacroBuilder.java
@@ -7,11 +7,25 @@ import net.minecraft.server.command.ServerCommandSource;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Objects;
+import java.util.function.Consumer;
 import java.util.function.Function;
 
 public class MacroBuilder {
     private final Map<String, ToDoubleFunction<ServerCommandSource>> arithmeticFunctions = new HashMap<>();
     private final Map<String, Function<ServerCommandSource, String>> stringFunctions = new HashMap<>();
+
+    public static Consumer<MacroBuilder> forEntity() {
+        return builder -> builder
+                .string("uuid", source -> Objects.requireNonNull(source.getEntity()).getUuidAsString())
+                .arithmetic("vel/x", source -> Objects.requireNonNull(source.getEntity()).getVelocity().x)
+                .arithmetic("vel/y", source -> Objects.requireNonNull(source.getEntity()).getVelocity().y)
+                .arithmetic("vel/z", source -> Objects.requireNonNull(source.getEntity()).getVelocity().z);
+    }
+
+    public static Consumer<MacroBuilder> forEntity(Consumer<MacroBuilder> consumer) {
+        return forEntity().andThen(consumer);
+    }
 
     public MacroBuilder arithmetic(String field, ToDoubleFunction<ServerCommandSource> function) {
         var old = arithmeticFunctions.put(field, function);
