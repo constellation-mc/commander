94e939e254675f060c12f6dccc4aedf09941957e
melontini <104443436+melontini@users.noreply.github.com>
2024-04-07 20:35:05 +0700

    Support arithmetica in RandomCommand

--- src/main/java/me/melontini/commander/impl/builtin/commands/logic/RandomCommand.java
+++ src/main/java/me/melontini/commander/impl/builtin/commands/logic/RandomCommand.java
@@ -5,21 +5,23 @@ import com.mojang.serialization.codecs.RecordCodecBuilder;
 import me.melontini.commander.api.command.Command;
 import me.melontini.commander.api.command.CommandType;
 import me.melontini.commander.api.event.EventContext;
+import me.melontini.commander.api.expression.Arithmetica;
 import me.melontini.commander.impl.builtin.BuiltInCommands;
 import me.melontini.dark_matter.api.data.codecs.ExtraCodecs;
 import net.minecraft.util.collection.WeightedList;
 
-public record RandomCommand(WeightedList<Command.Conditioned> commands, int rolls) implements Command {
+public record RandomCommand(WeightedList<Command.Conditioned> commands, Arithmetica rolls) implements Command {
 
     public static final Codec<RandomCommand> CODEC = RecordCodecBuilder.create(data -> data.group(
                 ExtraCodecs.weightedList(Command.CODEC).fieldOf("commands").forGetter(RandomCommand::commands),
-                ExtraCodecs.optional("rolls", Codec.INT, 1).forGetter(RandomCommand::rolls)
+                ExtraCodecs.optional("rolls", Arithmetica.CODEC, Arithmetica.constant(1)).forGetter(RandomCommand::rolls)
         ).apply(data, RandomCommand::new));
 
     @Override
     public boolean execute(EventContext context) {
         boolean b = false;
-        for (int i = 0; i < rolls(); i++) {
+        int rolls = rolls().asInt(context.lootContext());
+        for (int i = 0; i < rolls; i++) {
             var itr = this.commands().shuffle().iterator();
             if (itr.hasNext()) b |= itr.next().execute(context);
         }
