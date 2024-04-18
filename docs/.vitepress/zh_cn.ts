5c8866e4a7fcd18297538caf225c9f00753ad2d7
melontini <104443436+melontini@users.noreply.github.com>
2024-04-19 00:49:32 +0700

    split off search provider

--- docs/.vitepress/zh_cn.ts
+++ docs/.vitepress/zh_cn.ts
@@ -15,6 +15,30 @@ export const zh_cn = defineConfig({
                     { text: '算术', link: '/zh-cn/Arithmetica' }
                 ]
             }
-        ]
+        ],
+
+        search: {
+            provider: 'local',
+            options: {
+                locales: {
+                    "zh-cn": {
+                        translations: {
+                            button: {
+                                buttonText: '搜索文档',
+                                buttonAriaLabel: '搜索文档'
+                            },
+                            modal: {
+                                noResultsText: '无法找到相关结果',
+                                resetButtonTitle: '清除查询条件',
+                                footer: {
+                                    selectText: '选择',
+                                    navigateText: '切换'
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
     }
 })
