033932549bc9a186dc072381fd34cd2fe0287722
melontini <104443436+melontini@users.noreply.github.com>
2024-05-05 15:35:25 +0700

    Move BooleanExpression to Commander.

--- src/main/java/me/melontini/commander/impl/util/mappings/MinecraftDownloader.java
+++ src/main/java/me/melontini/commander/impl/util/mappings/MinecraftDownloader.java
@@ -36,11 +36,11 @@ public final class MinecraftDownloader {
 
             log.info("Downloading {}...", Objects.requireNonNull(mappings.getFileName()).toString());
             Files.writeString(parent.resolve("LICENSE.txt"), """
-                     (c) 2020 Microsoft Corporation.
-                     These mappings are provided "as-is" and you bear the risk of using them.
-                     You may copy and use the mappings for development purposes, but you may not redistribute the mappings complete and unmodified.
-                     Microsoft makes no warranties, express or implied, with respect to the mappings provided here.
-                     Use and modification of this document or the source code (in any form) of Minecraft: Java Edition is governed by the Minecraft End User License Agreement available at https://account.mojang.com/documents/minecraft_eula.
+                    (c) 2020 Microsoft Corporation.
+                    These mappings are provided "as-is" and you bear the risk of using them.
+                    You may copy and use the mappings for development purposes, but you may not redistribute the mappings complete and unmodified.
+                    Microsoft makes no warranties, express or implied, with respect to the mappings provided here.
+                    Use and modification of this document or the source code (in any form) of Minecraft: Java Edition is governed by the Minecraft End User License Agreement available at https://account.mojang.com/documents/minecraft_eula.
                     """);
             try (var defStream = new DeflaterInputStream(stream)) {
                 Files.write(mappings, defStream.readAllBytes());
