5f88350fd52783f7fae47630fa359acc170e3838
melontini <104443436+melontini@users.noreply.github.com>
2024-05-05 14:02:25 +0700

    Revert "Ugh... I'm not sure if I want to do this."
    This reverts commit e0cf7f82

--- src/main/java/me/melontini/commander/impl/Commander.java
+++ src/main/java/me/melontini/commander/impl/Commander.java
@@ -1,5 +1,7 @@
 package me.melontini.commander.impl;
 
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
 import lombok.Getter;
 import lombok.experimental.Accessors;
 import lombok.extern.log4j.Log4j2;
@@ -13,6 +15,7 @@ import me.melontini.commander.impl.util.loot.ArithmeticaLootNumberProvider;
 import me.melontini.commander.impl.util.loot.ExpressionLootCondition;
 import me.melontini.commander.impl.util.mappings.AmbiguousRemapper;
 import me.melontini.commander.impl.util.mappings.MappingKeeper;
+import me.melontini.commander.impl.util.mappings.MinecraftDownloader;
 import me.melontini.dark_matter.api.base.util.Exceptions;
 import me.melontini.dark_matter.api.base.util.MakeSure;
 import me.melontini.dark_matter.api.base.util.PrependingLogger;
@@ -29,6 +32,7 @@ import net.minecraft.util.Identifier;
 import net.minecraft.util.Util;
 
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.nio.file.Files;
 import java.nio.file.LinkOption;
 import java.nio.file.Path;
@@ -45,6 +49,7 @@ public class Commander {
     public static final LootConditionType EXPRESSION_CONDITION = Registry.register(Registries.LOOT_CONDITION_TYPE, id("expression"), new LootConditionType(ExtraCodecs.toJsonSerializer(ExpressionLootCondition.CODEC.codec())));
 
     public static final Path COMMANDER_PATH = FabricLoader.getInstance().getGameDir().resolve(".commander");
+    public static final String MINECRAFT_VERSION = getVersion();
 
     @Getter
     private AmbiguousRemapper mappingKeeper;
@@ -79,11 +84,13 @@ public class Commander {
         EvalUtils.init();
 
         try {
+            MinecraftDownloader.downloadMappings();
+
             CompletableFuture<MemoryMappingTree> offMojmap = CompletableFuture.supplyAsync(MappingKeeper::loadOffMojmap, Util.getMainWorkerExecutor());
             CompletableFuture<MemoryMappingTree> offTarget = CompletableFuture.supplyAsync(MappingKeeper::loadOffTarget, Util.getMainWorkerExecutor());
             mappingKeeper = new MappingKeeper(MappingKeeper.loadMojmapTarget(offMojmap.join(), offTarget.join()));
         } catch (Throwable t) {
-            log.error("Failed to prepare mappings! Data access remapping will not work!!!", t);
+            log.error("Failed to download and prepare mappings! Data access remapping will not work!!!", t);
             mappingKeeper = (cls, name) -> name;//Returning null will force it to traverse the hierarchy.
         }
 
@@ -97,5 +104,10 @@ public class Commander {
                 KILLER_ENTITY, DIRECT_KILLER_ENTITY,
                 DAMAGE_SOURCE, EXPLOSION_RADIUS,
                 BLOCK_STATE, BLOCK_ENTITY);
+    }
+
+    private static String getVersion() {
+        JsonObject o = JsonParser.parseReader(new InputStreamReader(MinecraftDownloader.class.getResourceAsStream("/version.json"))).getAsJsonObject();
+        return o.getAsJsonPrimitive("id").getAsString();
     }
 }
