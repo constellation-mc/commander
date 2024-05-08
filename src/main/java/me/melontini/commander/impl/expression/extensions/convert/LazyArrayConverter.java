c16e12e1055ac150e2c307e782c4a43b11acf712
melontini <104443436+melontini@users.noreply.github.com>
2024-05-09 00:45:26 +0700

    Lazy array evaluation. (Hopefully)

--- src/main/java/me/melontini/commander/impl/expression/extensions/convert/LazyArrayConverter.java
+++ src/main/java/me/melontini/commander/impl/expression/extensions/convert/LazyArrayConverter.java
@@ -2,7 +2,6 @@ package me.melontini.commander.impl.expression.extensions.convert;
 
 import com.ezylang.evalex.config.ExpressionConfiguration;
 import com.ezylang.evalex.data.EvaluationValue;
-import com.ezylang.evalex.data.conversion.ArrayConverter;
 import com.ezylang.evalex.data.conversion.ConverterIfc;
 import com.google.common.collect.Lists;
 import me.melontini.commander.impl.expression.extensions.ProxyMap;
@@ -11,12 +10,32 @@ import java.util.List;
 
 public class LazyArrayConverter implements ConverterIfc {
 
-    private final ArrayConverter converter = new ArrayConverter();
-
     @Override
     public EvaluationValue convert(Object object, ExpressionConfiguration configuration) {
         if (object instanceof List<?> l) return EvaluationValue.arrayValue(Lists.transform(l, ProxyMap::convert));
-        return converter.convert(object, configuration);
+        return convertArray(object);
+    }
+
+    private EvaluationValue convertArray(Object array) {
+        if (array instanceof int[] arr) {
+            return EvaluationValue.arrayValue(new LazyArrayWrappers.IntArray(arr));
+        } else if (array instanceof long[] arr) {
+            return EvaluationValue.arrayValue(new LazyArrayWrappers.LongArray(arr));
+        } else if (array instanceof double[] arr) {
+            return EvaluationValue.arrayValue(new LazyArrayWrappers.DoubleArray(arr));
+        } else if (array instanceof float[] arr) {
+            return EvaluationValue.arrayValue(new LazyArrayWrappers.FloatArray(arr));
+        } else if (array instanceof short[] arr) {
+            return EvaluationValue.arrayValue(new LazyArrayWrappers.ShortArray(arr));
+        } else if (array instanceof char[] arr) {
+            return EvaluationValue.arrayValue(new LazyArrayWrappers.CharArray(arr));
+        } else if (array instanceof byte[] arr) {
+            return EvaluationValue.arrayValue(new LazyArrayWrappers.ByteArray(arr));
+        } else if (array instanceof boolean[] arr) {
+            return EvaluationValue.arrayValue(new LazyArrayWrappers.BooleanArray(arr));
+        } else {
+            return EvaluationValue.arrayValue(new LazyArrayWrappers.ObjectArray((Object[]) array));
+        }
     }
 
     @Override
