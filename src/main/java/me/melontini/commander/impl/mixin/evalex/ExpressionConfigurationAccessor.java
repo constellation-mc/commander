2e2a770a749b81f1797ff051f1703a05a0b30086
melontini <104443436+melontini@users.noreply.github.com>
2024-05-08 23:41:43 +0700

    Completely remove case-insensitive constants.

---
+++ src/main/java/me/melontini/commander/impl/mixin/evalex/ExpressionConfigurationAccessor.java
@@ -0,0 +1,17 @@
+package me.melontini.commander.impl.mixin.evalex;
+
+import com.ezylang.evalex.config.ExpressionConfiguration;
+import com.ezylang.evalex.data.EvaluationValue;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Mutable;
+import org.spongepowered.asm.mixin.gen.Accessor;
+
+import java.util.Map;
+
+@Mixin(ExpressionConfiguration.class)
+public interface ExpressionConfigurationAccessor {
+
+    @Mutable
+    @Accessor("defaultConstants")
+    void commander$defaultConstants(Map<String, EvaluationValue> map);
+}
---
+++ src/main/java/me/melontini/commander/impl/mixin/evalex/MapBasedFunctionDictionaryAccessor.java
@@ -0,0 +1,15 @@
+package me.melontini.commander.impl.mixin.evalex;
+
+import com.ezylang.evalex.config.MapBasedFunctionDictionary;
+import com.ezylang.evalex.functions.FunctionIfc;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.gen.Accessor;
+
+import java.util.Map;
+
+@Mixin(MapBasedFunctionDictionary.class)
+public interface MapBasedFunctionDictionaryAccessor {
+
+    @Accessor("functions")
+    Map<String, FunctionIfc> commander$getFunctions();
+}
