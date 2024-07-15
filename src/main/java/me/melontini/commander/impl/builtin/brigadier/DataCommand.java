f83f00c74f798a418c68639d4a38939fae02c2fc
melontini <104443436+melontini@users.noreply.github.com>
2024-07-15 20:37:11 +0700

    Add `remove` to `cmd:data` and greatly simplify logic. - Return a constant NbtCompound in stack nbt and use NbtPredicate to convert entities to nbt.

--- src/main/java/me/melontini/commander/impl/builtin/brigadier/DataCommand.java
+++ src/main/java/me/melontini/commander/impl/builtin/brigadier/DataCommand.java
@@ -3,6 +3,10 @@ package me.melontini.commander.impl.builtin.brigadier;
 import com.mojang.brigadier.CommandDispatcher;
 import com.mojang.brigadier.arguments.StringArgumentType;
 import com.mojang.brigadier.builder.RequiredArgumentBuilder;
+import com.mojang.brigadier.context.CommandContext;
+import com.mojang.brigadier.exceptions.CommandSyntaxException;
+import com.mojang.brigadier.exceptions.DynamicCommandExceptionType;
+import com.mojang.brigadier.exceptions.SimpleCommandExceptionType;
 import me.melontini.commander.impl.Commander;
 import me.melontini.commander.impl.builtin.commands.StoreDataCommand.Target;
 import me.melontini.dark_matter.api.minecraft.util.TextUtil;
@@ -22,59 +26,28 @@ import java.util.function.Supplier;
 
 public class DataCommand {
 
-    private static final Map<Target, Supplier<RequiredArgumentBuilder<ServerCommandSource, ?>>> READ = Map.of(
-            Target.LEVEL, () -> CommandManager.argument("key", StringArgumentType.string())
-                    .executes(context -> readValue(context.getSource(), context.getSource().getWorld(), StringArgumentType.getString(context, "key"))),
+    private static final DynamicCommandExceptionType NO_BE_EXCEPTION = new DynamicCommandExceptionType((a) -> TextUtil.literal("No block entity at position %s".formatted(a)));
+    private static final DynamicCommandExceptionType NO_KEY_EXCEPTION = new DynamicCommandExceptionType((a) -> TextUtil.literal("No such key '%s' in target".formatted(a)));
+    private static final SimpleCommandExceptionType WRONG_DATA_TYPE_EXCEPTION = new SimpleCommandExceptionType(TextUtil.literal("Nbt element must be a string or a number!"));
 
-            Target.CHUNK, () -> CommandManager.argument("position", BlockPosArgumentType.blockPos())
-                    .then(CommandManager.argument("key", StringArgumentType.string())
-                            .executes(context -> readValue(context.getSource(), context.getSource().getWorld().getChunk(BlockPosArgumentType.getBlockPos(context, "position")), StringArgumentType.getString(context, "key")))),
-
-            Target.ENTITY, () -> CommandManager.argument("entity", EntityArgumentType.entity())
-                    .then(CommandManager.argument("key", StringArgumentType.string())
-                            .executes(context -> readValue(context.getSource(), EntityArgumentType.getEntity(context, "entity"), StringArgumentType.getString(context, "key")))),
-
+    private static final Map<Target, Supplier<RequiredArgumentBuilder<ServerCommandSource, ?>>> ARGS = Map.of(
+            Target.LEVEL, () -> null,
+            Target.CHUNK, () -> CommandManager.argument("position", BlockPosArgumentType.blockPos()),
+            Target.ENTITY, () -> CommandManager.argument("entity", EntityArgumentType.entity()),
             Target.BLOCK_ENTITY, () -> CommandManager.argument("position", BlockPosArgumentType.blockPos())
-                    .then(CommandManager.argument("key", StringArgumentType.string())
-                            .executes(context -> {
-                                var pos = BlockPosArgumentType.getBlockPos(context, "position");
-                                var be = context.getSource().getWorld().getBlockEntity(pos);
-                                if (be == null) {
-                                    context.getSource().sendError(TextUtil.literal("No block entity at position '%s %s %s'".formatted(pos.getX(), pos.getY(), pos.getZ())));
-                                    return 0;
-                                }
-                                return readValue(context.getSource(), be, StringArgumentType.getString(context, "key"));
-                            }))
     );
 
-    private static final Map<Target, Supplier<RequiredArgumentBuilder<ServerCommandSource, ?>>> WRITE = Map.of(
-            Target.LEVEL, () -> CommandManager.argument("key", StringArgumentType.string())
-                    .then(CommandManager.argument("data", NbtElementArgumentType.nbtElement())
-                            .executes(context -> writeValue(context.getSource(), context.getSource().getWorld(), StringArgumentType.getString(context, "key"), NbtElementArgumentType.getNbtElement(context, "data")))),
-
-            Target.CHUNK, () -> CommandManager.argument("position", BlockPosArgumentType.blockPos())
-                    .then(CommandManager.argument("key", StringArgumentType.string())
-                            .then(CommandManager.argument("data", NbtElementArgumentType.nbtElement())
-                                    .executes(context -> writeValue(context.getSource(), context.getSource().getWorld().getChunk(BlockPosArgumentType.getBlockPos(context, "position")), StringArgumentType.getString(context, "key"), NbtElementArgumentType.getNbtElement(context, "data"))))),
-
-            Target.ENTITY, () -> CommandManager.argument("entity", EntityArgumentType.entity())
-                    .then(CommandManager.argument("key", StringArgumentType.string())
-                            .then(CommandManager.argument("data", NbtElementArgumentType.nbtElement())
-                                    .executes(context -> writeValue(context.getSource(), EntityArgumentType.getEntity(context, "entity"), StringArgumentType.getString(context, "key"), NbtElementArgumentType.getNbtElement(context, "data"))))),
-
-            Target.BLOCK_ENTITY, () -> CommandManager.argument("position", BlockPosArgumentType.blockPos())
-                    .then(CommandManager.argument("key", StringArgumentType.string())
-                            .then(CommandManager.argument("data", NbtElementArgumentType.nbtElement())
-                                    .executes(context -> {
-                                        var pos = BlockPosArgumentType.getBlockPos(context, "position");
-                                        var be = context.getSource().getWorld().getBlockEntity(pos);
-                                        if (be == null) {
-                                            context.getSource().sendError(TextUtil.literal("No block entity at position '%s %s %s'".formatted(pos.getX(), pos.getY(), pos.getZ())));
-                                            return 0;
-                                        }
-                                        return writeValue(context.getSource(), be, StringArgumentType.getString(context, "key"), NbtElementArgumentType.getNbtElement(context, "data"));
-                                    })))
-
+    private static final Map<Target, PseudoFunction<CommandContext<ServerCommandSource>, AttachmentTarget>> TO_TARGET = Map.of(
+            Target.LEVEL, ctx -> ctx.getSource().getWorld(),
+            Target.CHUNK, ctx -> ctx.getSource().getWorld().getChunk(BlockPosArgumentType.getBlockPos(ctx, "position")),
+            Target.ENTITY, ctx -> EntityArgumentType.getEntity(ctx, "entity"),
+            Target.BLOCK_ENTITY, ctx -> {
+                var pos = BlockPosArgumentType.getBlockPos(ctx, "position");
+                var be = ctx.getSource().getWorld().getBlockEntity(pos);
+                if (be == null)
+                    throw NO_BE_EXCEPTION.create("[%s, %s, %s]".formatted(pos.getX(), pos.getY(), pos.getZ()));
+                return be;
+            }
     );
 
     public static void register(CommandDispatcher<ServerCommandSource> dispatcher) {
@@ -82,36 +55,80 @@ public class DataCommand {
 
         var read = CommandManager.literal("read");
         for (Target value : Target.values()) {
-            read.then(CommandManager.literal(value.name().toLowerCase(Locale.ROOT)).then(READ.get(value).get()));
+            var start = CommandManager.literal(value.name().toLowerCase(Locale.ROOT));
+            var keyArg = CommandManager.argument("key", StringArgumentType.string())
+                    .executes(context -> readValue(context.getSource(), TO_TARGET.get(value).apply(context), StringArgumentType.getString(context, "key")));
+
+            var args = ARGS.get(value).get();
+            if (args != null) args.then(keyArg);
+            else args = keyArg;
+
+            read.then(start.then(args));
         }
         cmd.then(read);
 
         var write = CommandManager.literal("write");
         for (Target value : Target.values()) {
-            write.then(CommandManager.literal(value.name().toLowerCase(Locale.ROOT)).then(WRITE.get(value).get()));
+            var start = CommandManager.literal(value.name().toLowerCase(Locale.ROOT));
+            var keyArg = CommandManager.argument("key", StringArgumentType.string()).then(CommandManager.argument("data", NbtElementArgumentType.nbtElement())
+                    .executes(context -> writeValue(context.getSource(), TO_TARGET.get(value).apply(context), StringArgumentType.getString(context, "key"), NbtElementArgumentType.getNbtElement(context, "data"))));
+
+            var args = ARGS.get(value).get();
+            if (args != null) args.then(keyArg);
+            else args = keyArg;
+
+            write.then(start.then(args));
         }
         cmd.then(write);
+
+        var remove = CommandManager.literal("remove");
+        for (Target value : Target.values()) {
+            var start = CommandManager.literal(value.name().toLowerCase(Locale.ROOT));
+            var keyArg = CommandManager.argument("key", StringArgumentType.string())
+                    .executes(context -> removeValue(context.getSource(), TO_TARGET.get(value).apply(context), StringArgumentType.getString(context, "key")));
+
+            var args = ARGS.get(value).get();
+            if (args != null) args.then(keyArg);
+            else args = keyArg;
+
+            remove.then(start.then(args));
+        }
+        cmd.then(remove);
 
         dispatcher.register(cmd);
     }
 
-    public static int writeValue(ServerCommandSource source, AttachmentTarget target, String key, NbtElement element) {
-        var r = target.getAttachedOrCreate(Commander.DATA_ATTACHMENT);
-        if (element instanceof AbstractNbtNumber || element instanceof NbtString) {
-            r.put(key, element);
+    private static int removeValue(ServerCommandSource source, AttachmentTarget target, String key) {
+        var nbt = target.getAttachedOrCreate(Commander.DATA_ATTACHMENT);
+        if (nbt.contains(key)) {
+            nbt.remove(key);
+            source.sendFeedback(() -> TextUtil.literal("Successfully removed key %s from target!".formatted(key)), false);
             return 1;
         }
-        source.sendError(TextUtil.literal("Nbt element must be a string or a number!"));
+        source.sendFeedback(() -> TextUtil.literal("Target has no %s key!".formatted(key)), false);
         return 0;
     }
 
-    public static int readValue(ServerCommandSource source, AttachmentTarget target, String key) {
+    public static int writeValue(ServerCommandSource source, AttachmentTarget target, String key, NbtElement element) throws CommandSyntaxException {
+        var r = target.getAttachedOrCreate(Commander.DATA_ATTACHMENT);
+        if (element instanceof AbstractNbtNumber || element instanceof NbtString) {
+            r.put(key, element);
+            source.sendFeedback(() -> TextUtil.literal("Successfully wrote value %s to target!".formatted(element)), false);
+            return 1;
+        }
+        throw WRONG_DATA_TYPE_EXCEPTION.create();
+    }
+
+    public static int readValue(ServerCommandSource source, AttachmentTarget target, String key) throws CommandSyntaxException {
         var r = target.getAttachedOrCreate(Commander.DATA_ATTACHMENT).get(key);
         if (r == null) {
-            source.sendError(TextUtil.literal("No such key '%s'".formatted(key)));
-            return 0;
+            throw NO_KEY_EXCEPTION.create(key);
         }
-        source.sendMessage(TextUtil.literal(r.asString()));
+        source.sendFeedback(() -> TextUtil.literal(r.asString()), false);
         return 1;
+    }
+
+    private interface PseudoFunction<T, R> {
+        R apply(T t) throws CommandSyntaxException;
     }
 }
--- src/main/java/me/melontini/commander/impl/expression/extensions/CustomFields.java
+++ src/main/java/me/melontini/commander/impl/expression/extensions/CustomFields.java
@@ -9,20 +9,19 @@ import net.minecraft.entity.Entity;
 import net.minecraft.entity.LivingEntity;
 import net.minecraft.item.ItemStack;
 import net.minecraft.nbt.NbtCompound;
+import net.minecraft.predicate.NbtPredicate;
 import net.minecraft.state.State;
 
 class CustomFields {
 
+    private static final NbtCompound EMPTY = new NbtCompound();
+
     static void init() {
         ReflectiveMapStructure.addField(ItemStack.class, "nbt", object -> {
-            if (object.getNbt() == null) return new NbtCompound();
+            if (object.getNbt() == null) return EMPTY;
             return object.getNbt();
         });
-        ReflectiveMapStructure.addField(Entity.class, "nbt", object -> {
-            NbtCompound compound = new NbtCompound();
-            object.writeNbt(compound);
-            return compound;
-        });
+        ReflectiveMapStructure.addField(Entity.class, "nbt", NbtPredicate::entityToNbt);
         ReflectiveMapStructure.addField(BlockEntity.class, "nbt", BlockEntity::createNbtWithIdentifyingData);
 
         ReflectiveMapStructure.addField(State.class, "properties", StateStruct::new);
