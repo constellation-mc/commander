f346fe6fc7e6addec1b49ca7777c9c7a00855ac2
melontini <104443436+melontini@users.noreply.github.com>
2024-08-04 23:04:31 +0700

    Unused x2.

--- src/main/java/me/melontini/commander/impl/util/DataTarget.java
+++ src/main/java/me/melontini/commander/impl/util/DataTarget.java
@@ -1,28 +1,8 @@
 package me.melontini.commander.impl.util;
 
-import java.util.Locale;
-import java.util.Objects;
-import java.util.function.Function;
-import net.fabricmc.fabric.api.attachment.v1.AttachmentTarget;
-import net.minecraft.server.command.ServerCommandSource;
-import net.minecraft.util.math.BlockPos;
-
 public enum DataTarget {
-  LEVEL(ServerCommandSource::getWorld),
-  CHUNK(source -> source.getWorld().getChunk(BlockPos.ofFloored(source.getPosition()))),
-  ENTITY(ServerCommandSource::getEntity),
-  BLOCK_ENTITY(
-      source -> source.getWorld().getBlockEntity(BlockPos.ofFloored(source.getPosition())));
-
-  private final Function<ServerCommandSource, AttachmentTarget> function;
-
-  DataTarget(Function<ServerCommandSource, AttachmentTarget> function) {
-    this.function = function;
-  }
-
-  public AttachmentTarget select(ServerCommandSource source) {
-    return Objects.requireNonNull(
-        this.function.apply(source),
-        () -> "Source cannot be null! Type: %s".formatted(this.name().toLowerCase(Locale.ROOT)));
-  }
+  LEVEL,
+  CHUNK,
+  ENTITY,
+  BLOCK_ENTITY
 }
